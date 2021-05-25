1f3e58ecaca7f4433b66b170f240dd6dd0688f19
peustratt <peustratt@gmail.com>
2021-05-24 23:18:42 -0300

    Adicionado teste de enviador de email, seguindo o conceito de TDD e baby steps

---
+++ libpythonpro/tests/test_spam/test_enviador_de_email.py
@@ -0,0 +1,36 @@
+import pytest
+
+from libpythonpro.spam.enviador_de_email import Enviador, EmailInvalido
+
+
+def test_criar_enviador_de_email():
+    enviador = Enviador()
+    assert enviador is not None
+
+
+@pytest.mark.parametrize(
+    'remetente',
+    ['renzo@pythonpro.br', 'foo@bar.com.br']
+)
+def test_remente(remetente):
+    enviador = Enviador()
+    resultado = enviador.enviar(
+        remetente,
+        'luciano@python.pro.br',
+        'Cursos Python Pro',
+        'Primeira turma Guido Von Rossum aberta.')
+    assert remetente in resultado
+
+
+@pytest.mark.parametrize(
+    'remetente',
+    ['', 'renzo']
+)
+def test_remente_invalido(remetente):
+    enviador = Enviador()
+    with pytest.raises(EmailInvalido):
+        resultado = enviador.enviar(
+            remetente,
+            'luciano@python.pro.br',
+            'Cursos Python Pro',
+            'Primeira turma Guido Von Rossum aberta.')
